06fc7c0f8244747b17de7c29c141e6760504bf6c
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-07-12 11:08:38 +0800

    user features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,31 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.create(user_params)
+   
+   # puts @user.errors.full_messages
+    if @user.valid?
+      redirect_to "/sessions/new"
+    else
+      flash[:alert] = @user.errors.full_messages
+      redirect_to "/users/new"
+    end
+
+   
+    
+  end
+
   def show
     @user = User.find(params[:id])
   end
 
   def edit
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:confirm_password)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
   get 'users/:id' =>  'users#show'
+  post 'users/create'
   # get 'users/new'
   # get 'users/show'
   

8dd0d858e6731243a4f9e01359836365a2c6e7ef
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-07-12 16:04:44 +0800

    for secrets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :secrets
+  has_many :likes, dependent: :destroy
+  #has_many :secrets, through: :likes
+  #we're going to have to give an alias to one of our has_many relationships and use :source to explicitly tell Rails which table we're referring to.
+  has_many :secrets_liked, through: :likes, source: :secret
+
   email_format = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :name, :email, :password_digest , presence: true
   validates :email, format: {with: email_format}
